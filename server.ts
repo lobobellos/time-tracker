5f749ef81b21ca871ddae1e9bf82e3e47a4a6284
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-02 08:49:16 -0600

    turn all gets into the main index.js

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import * as url from 'url';
 import path from 'node:path';
 import chalk from 'chalk';
-import { getRawData } from './dataManager';
+import { getRawData } from './dataManager.js';
 
 let app = express();
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -12,15 +12,15 @@ const buildPath = path.resolve(__dirname, 'dist');
 
 app.use(express.static(buildPath));
 
-
-app.get('/', (req,res)=> {
-  res.sendFile(buildPath + "/index.html");
-});
-
 app.get("/data", (req, res) => {
   res.type("json");
   res.send(getRawData());
 });
+
+app.get('*', (req,res)=> {
+  res.sendFile(buildPath + "/index.html");
+});
+
 
 const PORT = process.env.PORT || 8080;
 
