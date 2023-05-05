06b4057f815e86b85b68ca1ed506fdb3b2086146
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-05 16:59:08 -0600

    finalize get and set methods

--- dataManager.ts
+++ dataManager.ts
@@ -2,31 +2,42 @@ import fs from 'fs/promises';
 
 type time = [number, number];
 
-class UserData {
-  name: string;
-  age: number;
-  time: time[];
-  totalTime: number
-  constructor(name: string, age: number, time: time[]){
-    this.name = name;
-    this.age = age;
-    this.time = time;
-    this.totalTime =  time.reduce((a, b) => (a[1]-a[0]) + (b[1]-b[0]), 0);
-  }
+type User = [
+  pin: number,
+  data: UserData
+]
 
-  toString(): string{
-    return JSON.stringify({name: this.name, age: this.age, time: this.time});
-  }
+type UserData = [
+  name: string,
+  title: string,
+  times: time[],
+]
+
+export async function addUser(pin: number, name: string, title: string) {
+  writeData([
+    ...(await getRawData()),
+    <User>[pin,[name,title,[]]]
+  ])
 }
 
-export async function getRawData(): Promise<UserData[]>{
+export async function deleteUser(pin:number){
+  writeData(
+    (await getRawData()).filter(el=>el[0] != pin)
+  )
+}
+
+export async function getRawData(): Promise<User[]> {
   return JSON.parse(await fs.readFile('./data/data.json', 'utf-8'));
 }
 
-export async function getParsedData(){
-  return (await getRawData()).map(d => new UserData(d.name, d.age, d.time));
+export async function getParsedData() {
+  return new Map(await getRawData());
 }
 
-export function writeData(data: UserData[]){
-  return fs.writeFile('./data/data.json',data.toString() )
+export async function writeData(data: User[]) {
+  await fs.writeFile('./data/data.json', JSON.stringify(data.entries))
 }
+
+export async function writeDataFromMap(data: Map<number, UserData>) {
+  await writeData(Array.from(data.entries()))
+}   
