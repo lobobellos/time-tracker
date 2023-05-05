2a332a2748a9ccc6c63ffc7cade37af6af4555a3
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-05 14:17:43 -0600

    switch to edge-both

--- inputListener.ts
+++ inputListener.ts
@@ -4,7 +4,7 @@ gpio.on('change', (pin, value) => {
   console.log(pin, value)
 })
 
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_RISING)
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH)
 
 
 process.stdin.resume();
@@ -12,3 +12,9 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', data => {
   console.log(data);
 })
+
+process.on('SIGINT', function () {
+  console.log('Ctrl-C...');
+  gpio.destroy(()=>{console.log('gpio destroyed')})
+  process.exit(2);
+});
