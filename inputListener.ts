0ccdab5a6ab0165a3779652fa8f312ff0704f424
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-05 14:54:11 -0600

    made code more concise

--- inputListener.ts
+++ inputListener.ts
@@ -1,10 +1,8 @@
 import gpio from 'rpi-gpio'
 
-gpio.on('change', (pin, value) => {
-  console.log(pin, value)
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_RISING, () => {
+  console.log('button pressed')
 })
-
-gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH)
 
 
 process.stdin.resume();
