ad4e9833df448349a5b792e88858d43de9df9d60
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-07 15:52:01 -0600

    initial commit

---
+++ src/components/Top.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="item">
+      <router-link to="/">home</router-link>
+    </div>
+    <div class="item">
+      <router-link to="/createAcct">create an account</router-link>
+    </div>
+  </div>
+</template>
+
+
+<script lang="ts">
+
+export default {
+  name:"top",
+  data(){
+    return{
+      width: window.innerWidth
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+router-link{
+  font-family: 'Fira Sans', sans-serif;
+}
+
+.container{
+  display:flex;
+  flex-direction: row;
+}
+
+.item{
+  margin: 0.5rem;
+}
+</style>
