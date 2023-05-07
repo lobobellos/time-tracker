112871a507e1a5fd658d9f65cfdf0908579c6d9a
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-07 15:52:09 -0600

    import top component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+  import Top from './components/Top.vue'
 </script>
 
 <template>
   <div class="container">
+    <Top/>
     <router-view></router-view>
   </div>
 </template>
