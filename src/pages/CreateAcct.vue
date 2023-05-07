490e8e7b6e69d2d7b404a071d008cf2be63a8139
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-07 14:57:59 -0600

    use fancier vue features

--- src/pages/CreateAcct.vue
+++ src/pages/CreateAcct.vue
@@ -1,18 +1,18 @@
 <template>
   <h1>account creation</h1>
 
-  <form type="POST" >
+  <form type="POST">
     <label for="name">name (preferably your real one)</label><br>
-    <input type="text" name="name" required/><br>
+    <input type="text" name="name" v-model="name" required /><br>
     
     <label for="title">title (your job on the team)</label><br>
-    <input type="text" name="title"/><br>
+    <input type="text" name="title" v-model="title" required /><br>
     
     <label for="pin">pin</label><br>
-    <input type="number" name="pin" min="0" max ="9999"/><br>
+    <input type="number" name="pin" min="0" max ="9999" v-model="pin" required /><br>
 
     <label for="pin2">retype pin</label><br>
-    <input type="number" name="pin2" min="0" max ="9999"/><br>
+    <input type="number" name="pin2" min="0" max ="9999" v-model="pin2" required /><br>
 
     <input type="submit" value="submit">
   </form>
@@ -20,40 +20,44 @@
 
 
 <script lang="ts">
-let form: HTMLFormElement;
-window.onload = ()=>{
-  form = document.forms[0]
-  form.onsubmit = async e=>{
-    e.preventDefault()
-    let data = new FormData(form)
-    if(data.get('pin') != data.get('pin2')){
-      alert('pin does not match')
-      return;
-    }
-    fetch('/create',{
-      method: 'POST',
-      headers:[
-        ['Content-Type', 'application/json'],
-      ],
-      body: JSON.stringify({
-        name: data.get('name'),
-        title: data.get('title'),
-        pin: data.get('pin'),
-      }),
-      
-    }).then(async res=>{
-      alert(res.ok?
-      "account created" :
-      await res.text())
-    })
-  }
-}
-
 export default {
   name: 'CreateAcct',
   data(){
     return{
-      
+      name:"",
+      title:"",
+      pin:0,
+      pin2:0,
+    }
+  },
+  mounted(){
+    this.initializeForm()
+  },
+  methods:{
+    async submitForm(e:Event){
+     
+      e.preventDefault()
+      let data = new FormData(e.target as HTMLFormElement)
+      if(data.get('pin') != data.get('pin2')){
+        alert('pin does not match')
+        return;
+      }
+      fetch('/create',{
+        method: 'POST',
+        headers:[
+          ['Content-Type', 'application/json'],
+        ],
+        body: JSON.stringify({
+          name: data.get('name'),
+          title: data.get('title'),
+          pin: data.get('pin'),
+        }),
+        
+      }).then(async res=>{
+        alert(res.ok?
+        "account created" :
+        await res.text())
+      })
     }
   },
   
