0ced84cab27787c2ed4ca3ed9427ceb4d459593f
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-07 20:27:46 -0600

    add button handling

--- inputListener.ts
+++ inputListener.ts
@@ -1,17 +1,57 @@
 import gpio from 'rpi-gpio'
+import { getRawData, writeData } from './src/dataManager'
 
 await gpio.promise.setup(7, gpio.DIR_IN, gpio.EDGE_RISING)
 
-gpio.on('change', pin => {
-  if (pin == 7) {
-    console.log('pressed')
+type pin = number
+type timestamp = number
+
+let lastPin:null|number = null
+let clockedIn= new Map<pin,timestamp>()
+
+gpio.on('change', async rpipin => {
+  if(lastPin == null){
+    return
+  }
+  if (rpipin == 7) {
+    //clock in
+    console.log('clock in')
+    clockedIn.set(lastPin, Date.now())
+
+  }else if (rpipin == 8) {
+    //clock out
+    console.log('clock out')
+
+    if(clockedIn.has(lastPin)){
+      //@ts-ignore
+      
+    }
+    await writeData(
+      (await getRawData())
+      .map(el=>el[0] == lastPin ?
+        [
+          el[0],
+          el[1],
+          el[2],
+          [
+            ...el[3],
+            [<number>clockedIn.get(lastPin), Date.now()],
+          ],
+        ]: el
+      )
+    )
   }
 })
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
-process.stdin.on('data', data => {
-  console.log(data);
+process.stdin.on('data', (data:string) => {
+  lastPin = parseInt(data);
+  lastPin = lastPin >= 0 && lastPin < 10000 ? lastPin : null
+  console.log(lastPin)
+  setTimeout(() => {
+    lastPin = null
+  },15_000)
 })
 
 process.on('SIGINT', function () {
