0ccb7768324bc7643b0c3383d1ba21329cdd9df3
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-09 19:05:39 -0600

    add js-cookie

---
+++ src/pages/MyAcct.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ user }}</h1>
+</template>
+
+<script lang ="ts">
+import cookie from 'js-cookie'
+
+
+export default {
+  data(){
+    return {
+      user: null,
+    }
+  },
+  onMount(){
+    this.user = cookie.get("user")
+  }
+}
+
+</script>
