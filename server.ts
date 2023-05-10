9887ef537a75f4611bb9545ea14fb6a60e94f67b
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-09 20:54:49 -0600

    create login handler

--- server.ts
+++ server.ts
@@ -18,16 +18,36 @@ app.use(vite.middlewares)
 
 app.use(express.json());
 
-app.get("/data", async(req, res) => {
+app.get("/data", async (req, res) => {
   res.type("json");
   res.send(await getRawData());
 });
 
-app.post('/create',async (req, res) => {
-  try{
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  console.log(req.body.pin);
+  res.type("json");
+  const data = (await getRawData())
+    .find(el => el[0] == req.body.pin)
+    
+  if (!data) {
+    res.send(JSON.stringify({
+      found: false 
+    })) 
+  }else{
+    res.cookie("pin", req.body.pin);
+    res.send(JSON.stringify({
+      data,
+      found: true
+    }))
+  }
+});
+
+app.post('/create', async (req, res) => {
+  try {
     await addUser(req.body.pin, req.body.name, req.body.title);
     res.sendStatus(201);
-  }catch (err){
+  } catch (err) {
     res.status(400)
     res.send(err);
   }
@@ -42,7 +62,7 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
   console.log(chalk.greenBright(`Server is running on port ${PORT}.`));
   //@ts-ignore
-  console.log("\n IP addresses: \n"+Object.values((await import('os')).networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family==='IPv4' && !i.internal && i.address || []), [])), []).join("\n"));
+  console.log("\n IP addresses: \n" + Object.values((await import('os')).networkInterfaces()).reduce((r, list) => r.concat(list.reduce((rr, i) => rr.concat(i.family === 'IPv4' && !i.internal && i.address || []), [])), []).join("\n"));
 });
 
 
