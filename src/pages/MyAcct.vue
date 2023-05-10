2b89f37f5f7df20fc2853f353648c48eb0ec16a3
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-09 20:56:03 -0600

    first pass at a my-account page

--- src/pages/MyAcct.vue
+++ src/pages/MyAcct.vue
@@ -1,23 +1,42 @@
 <template>
   your account
-  <h1>{{ user }}</h1>
+  <h1>pin: {{ pin }}</h1>
+
+
+  user Info:{{ userInfo }}
 </template>
 
 <script lang ="ts">
 import cookie from 'js-cookie'
 
-
 export default {
   name:"my account",
   data(){
     return {
-      user: null,
+      pin: null,
+      userInfo:null,
     }
   },
   created(){
-    this.user = cookie.get("user")
+    this.pin = cookie.get("pin")
+    this.getData()
+  },
+  methods:{
+    async getData(){
 
-    alert(this.user)
+      this.userInfo = (await fetch('/login',{
+        method: 'POST',
+        headers:[
+          ['Content-Type', 'application/json'],
+        ],
+        body: JSON.stringify({
+          pin: this.pin
+        })
+      }).then(res=>res.json())).data
+
+
+      console.log(this.userInfo)
+    }
   }
 }
 
