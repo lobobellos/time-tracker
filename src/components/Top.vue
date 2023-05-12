f94868d289b099913164d7e26e397631665c749a
= <918780@dpsk12.net>
2023-05-11 21:19:32 -0600

    update navbar

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="container">
-    <div class="item">
-      <router-link to="/">Home</router-link>
+  <div class="container" v-if="width > 600">
+    <router-link to="/">
+      <img src="../assets/teamLogo.png" alt="team logo" id="logo">
+    </router-link>
+    <router-link to="/lookByDay">Specific data</router-link>
+    <router-link v-if="!isLoggedIn" to="/createAcct">Create Account</router-link>
+    <router-link v-if="!isLoggedIn" to="/login">Log in</router-link>
+    <router-link v-else to="/myAcct">My Account</router-link>
+  </div>
+  <div class="withDropdown" v-else>
+    <div class="container">
+      <router-link to="/">
+        <img src="../assets/teamLogo.png" alt="logo" id="logo">
+      </router-link>
+      <div class="threeLineContainer">
+        <img src="../assets/threeLines.svg" alt="pages" id="threeLines"
+          :style="{ transform: `rotate(${dropdown.rotation}deg)` }" 
+          @click="dropdownToggle" >
+      </div>
     </div>
-    <div class="item">
+    <div class="dropdown" v-if="dropdown.isOpen">
       <router-link to="/lookByDay">Specific data</router-link>
+      <router-link v-if="!isLoggedIn" to="/createAcct">Create Account</router-link>
+      <router-link v-if="!isLoggedIn" to="/login">Log in</router-link>
+      <router-link v-else to="/myAcct">My Account</router-link>
     </div>
-    <div class="item" v-if="!isLoggedIn">
-      <router-link to="/createAcct">Create Account</router-link>
-    </div>
-    <div class="item" v-if="!isLoggedIn">
-      <router-link to="/login">Log in</router-link>
-    </div>
-    <div class="item" v-else>
-      <router-link to="/myAcct">My Account</router-link>
-    </div>
-    
-
   </div>
 </template>
 
@@ -25,35 +33,74 @@
 import Cookies from 'js-cookie'
 
 export default {
-  name:"top",
-  data(){
-    return{
-      width: window.innerWidth,
-      isLoggedIn:Cookies.get('pin') != undefined
+  name: "top",
+  created() {
+    window.onresize = () => {
+      this.width = window.innerWidth
     }
   },
-  mounted(){
-    console.log(this.isLoggedIn)
-  }
+  data() {
+    return {
+      width: window.innerWidth,
+      isLoggedIn: Cookies.get('pin') != undefined,
+      dropdown: {
+        isOpen: false,
+        rotation: 0,
+      },
+    }
+  },
+  methods: {
+    dropdownToggle() {
+      this.dropdown.isOpen = !this.dropdown.isOpen
+      this.rotateImage()
+    },
+    rotateImage() {
+      this.dropdown.rotation = this.dropdown.rotation == 0 ? -90 : 0
+    }
+  },
 }
-
-
 </script>
 
 <style scoped>
-
-.container{
-  display:flex;
+.container {
+  display: flex;
   flex-direction: row;
   background-color: rgb(165, 109, 218);
 }
 
-.item{
-  margin: 0.5rem;
-  font-family: 'Fira Sans', sans-serif;
+.dropdown {
+  background-color: rgb(165, 109, 218);
+  padding: 0.5rem;
 }
 
-.item *{
+#logo {
+  align-self: left;
+  width: 50px;
+}
+
+.threeLineContainer {
+  display:flex;
+  margin-left: auto;
+  margin-right: 1rem;
+  align-items: center;
+}
+
+#threeLines {
+
+  width: 50px;
+  height:50px;
+  transition: transform 100ms ease-in-out;
+  clip: rect(0,50,0,50);
+}
+
+a {
+  margin-bottom: 0.5rem;
+  font-family: 'Fira Sans', sans-serif;
+  font-size: 1.5rem;
+  font-weight: 400;
+  display: flex;
+  align-items: flex-end;
+  text-align: bottom;
   text-decoration: none;
   color: rgb(255, 255, 255);
 }
