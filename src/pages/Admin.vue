184f72081b7be8357efd15cfce06b6813b05daaf
= <918780@dpsk12.net>
2023-05-17 17:22:40 -0600

    start on security features

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,10 +1,10 @@
 <template>
   <h2>Admin</h2>
   <label for="adminPass">admin password</label><br>
-  <input name="adminPass" type="password" v-model="adminPass">
+  <input name="adminPass" type="password" v-model="adminPass" @input="checkPass">
 
-  <div v-if="adminPass != ''">
-    <textarea name="" id="" ></textarea>
+  <div v-if="correctPass">
+    <textarea name="" id="" >{{ prettyJson }}</textarea>
     <button>sumbit new data</button>
   </div>
 </template>
@@ -12,6 +12,7 @@
 
 <script lang="ts">
 import { exampleData } from '../dataManager.js';
+import beautify from 'json-beautify';
 
 const day = 24* 60 *60 * 1000
 
@@ -21,13 +22,42 @@ export default {
   data(){
     return {
       adminPass : "",
+      correctPass : false,
+      data: [],
     }
+  },
+  computed: {
+    prettyJson() {
+      return beautify(this.data, null, 1000);
+    },
   },
   mounted(){
     
   },
   methods:{
-    
+    async checkPass(){
+      this.correctPass = this.correctPass ? true :
+      await fetch('/isAdminPassword', {
+        method: 'POST',
+        headers: [
+          ['Content-Type', 'application/json'],
+        ],
+        body: JSON.stringify({
+          password: this.adminPass,
+        })
+      })
+      .then(res=>res.json())
+    },
+    async getAllData(){
+      await fetch('/getAllData', {
+        method: 'GET',
+        headers: [
+          ['Content-Type', 'application/json'],
+          ['Accept', 'application/json'],
+          ['adminPassword', this.adminPass],
+        ],
+      })
+    }
   }
 }
 </script>
