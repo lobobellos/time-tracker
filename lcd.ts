7ef2a61092f0b4a7be68358959761d79ea77bd81
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-18 15:09:03 -0600

    format

--- lcd.ts
+++ lcd.ts
@@ -4,12 +4,12 @@ const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
 
 let ip = 'undefined'
 
-function getFullString(){
-  return  `go to ${ip}`
+function getFullString() {
+  return `go to ${ip}`
 }
 
-function getParsedString(){
-  return  getFullString().substring(cur, cur+16)
+function getParsedString() {
+  return getFullString().substring(cur, cur + 16)
 }
 
 let cur = 0
@@ -18,17 +18,15 @@ export function setIP(ipString: string) {
   ip = ipString
 }
 
-lcd.on('ready', async() => {
+lcd.on('ready', async () => {
   //lcd.autoscroll();
-  
+
   setInterval(async () => {
     await lcd.clear();
-
     lcd.setCursor(0, 0);
-    lcd.print( getParsedString(),()=>{});
-
+    lcd.print(getParsedString(), () => { });
     cur = (cur + 1) % (getFullString().length - 16)
-  },1000)
+  }, 1000)
 })
 
 
