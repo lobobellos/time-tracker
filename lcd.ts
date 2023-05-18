dc23ad38dd8f899bc38c6ff7de2a258afbdfe15e
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-18 11:34:19 -0600

    add lcd write function

--- lcd.ts
+++ lcd.ts
@@ -3,6 +3,7 @@ import Lcd from 'lcd';
 const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
 
 export async function writeToLcd(col, row, data) {
+  console.log("LCD written "+data)
   lcd.setCursor(col, row);
   lcd.print(data, (err) => {
     if (err) {
