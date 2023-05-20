b4d82db4a8719464a55291aef030e845cb91cfc0
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-05-20 14:33:41 -0600

    switch to using is_prod

--- lcd.ts
+++ lcd.ts
@@ -1,8 +1,23 @@
-import Lcd from 'lcd';
 
-const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
+if(process.env.IS_PROD == 'true'){
+  const Lcd =await import('lcd')
+  //@ts-ignore
+  const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
+
+  lcd.on('ready', async () => {
+    //lcd.autoscroll();
+  
+    setInterval(async () => {
+      await lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(getParsedString(), () => { });
+      cur = (cur + 1) % (getFullString().length - 16)
+    }, 1000)
+  })
+}
 
 let ip = 'undefined'
+let cur = 0
 
 function getFullString() {
   return `go to ${ip}`
@@ -12,21 +27,10 @@ function getParsedString() {
   return getFullString().substring(cur, cur + 16)
 }
 
-let cur = 0
-
 export function setIP(ipString: string) {
   ip = ipString
 }
 
-lcd.on('ready', async () => {
-  //lcd.autoscroll();
 
-  setInterval(async () => {
-    await lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print(getParsedString(), () => { });
-    cur = (cur + 1) % (getFullString().length - 16)
-  }, 1000)
-})
 
 
