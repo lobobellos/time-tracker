deac234dbc5440235c4549cd1e52d7550f92f852
= <918780@dpsk12.net>
2023-06-01 17:33:56 -0600

    directly start node in tmux

--- startTmux.sh
+++ startTmux.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-sudo -u pi tmux new -d -s timeTracker
+sudo tmux new -d -s timeTracker
 
-sudo -u pi tmux send-keys -t timeTracker:0 cd Space /home/pi/time-tracker/ C-m
-sudo -u pi tmux send-keys -t timeTracker:0 sudo Space ./startTracker.sh C-m
+sudo tmux send-keys -t timeTracker:0 cd Space /home/pi/time-tracker/ C-m
+sudo tmux send-keys -t timeTracker:0 sudo Space npm Space run Space start C-m
 echo "started tmux"
