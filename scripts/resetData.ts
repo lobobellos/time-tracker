67a73bf41aebd4dc7e0ac410482346b775e4b5b2
= <918780@dpsk12.net>
2023-06-01 09:53:02 -0600

    formatting

--- scripts/resetData.ts
+++ scripts/resetData.ts
@@ -2,4 +2,3 @@ import fs from 'fs/promises';
 
 await fs.mkdir('./data')
 await fs.writeFile('./data/data.json','[]','utf-8')
-
--- scripts/watchData.ts
+++ scripts/watchData.ts
@@ -2,16 +2,14 @@ import fs from 'fs/promises'
 import chalk from 'chalk'
 
 const path = './data/data.json'
-
 const watcher =fs.watch(path)
-
 console.log(chalk.blueBright('Watcher Started'))
 
-for await(let change of watcher){
+for await(let _ of watcher){
   console.clear()
   try{
     console.table(JSON.parse(await fs.readFile(path,'utf-8')))
-  }catch(err:SyntaxError|any){
-    console.error(err.message)
+  }catch(err){
+    console.error(err?.message)
   }
 }
