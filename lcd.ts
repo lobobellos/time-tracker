c51b98c30a72bced2619b114da79ed5f257f26d9
= <918780@dpsk12.net>
2023-06-01 09:51:20 -0600

    format

--- lcd.ts
+++ lcd.ts
@@ -2,23 +2,17 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export async function startLCD() {
-  
   if (process.env.IS_PROD == 'true') {
-  
     const Lcd = (await import('lcd')).default
-  
     const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
-  
     lcd.on('ready', async () => {
-      //lcd.autoscroll();
-  
       setInterval(async () => {
         await lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print(getParsedString(), () => {
           lcd.setCursor(0, 1);
-          lcd.print(line2 ?? "", () => {})
-         });
+          lcd.print(line2 ?? "", () => { })
+        });
         cur = (cur + 1) % (getFullString().length - 16)
       }, 1000)
     })
@@ -27,7 +21,7 @@ export async function startLCD() {
 
 let ip = 'undefined'
 let cur = 0
-let line2= ""
+let line2 = ""
 
 function getFullString() {
   return `go to ${ip}`
@@ -44,7 +38,3 @@ export function setIP(ipString: string) {
 export function setLine2(line2String: string) {
   line2 = line2String
 }
-
-
-
-
