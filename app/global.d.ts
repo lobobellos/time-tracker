9df7e4f0e3a4cfa8a9772ac5ae6913a67bbdccad
= <918780@dpsk12.net>
2023-07-27 18:19:16 -0600

    add more global types

---
+++ app/global.d.ts
@@ -0,0 +1,16 @@
+
+
+declare type privateUserData = [
+  name: string,
+  title: string,
+  times: time[]
+]
+
+declare type time = [number, number];
+
+declare type UserData = [
+  pin: number,
+  name: string,
+  title: string,
+  times: time[]
+]
