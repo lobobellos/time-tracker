1d923f11eb52e5ad182fa9b901eaf6968c0dfff5
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-29 12:32:00 -0600

    make global env safer

--- app/server/Global.ts
+++ app/server/Global.ts
@@ -4,11 +4,29 @@ import Client from '@replit/database'
 
 
 console.log("database URL: ",process.env?.REPLIT_DB_URL ? 'defined': 'undefined')
+console.log("admin password: ",process.env?.ADMIN_PASSWORD ? 'defined': 'undefined')
 
 if(process.env?.REPLIT_DB_URL == undefined) throw new Error('REPLIT_DB_URL is not defined')
+if(process.env?.ADMIN_PASSWORD == undefined) throw new Error('ADMIN_PASSWORD is not defined')
 
 export default class Global{
 
-  static db = new Client(process.env?.REPLIT_DB_URL)
+  private static _db = new Client(process.env?.REPLIT_DB_URL)
+  private static _adminPassword = process.env.ADMIN_PASSWORD
 
+  static get db(){
+    return Global._db
+  }
+
+  static set db(db){
+    throw new Error('cannot set this property')
+  }
+
+  static get adminPassword(){
+    return Global._adminPassword 
+  }
+
+  static set adminPassword(adminPassword){
+    throw new Error('cannot set this property')
+  }
 }
