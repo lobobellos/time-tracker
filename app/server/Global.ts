0cbcd0eae5279cc295115764a0afa83b1b84a8b3
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-29 12:41:54 -0600

    add fancy error handling

--- app/server/Global.ts
+++ app/server/Global.ts
@@ -2,12 +2,9 @@ import { config } from 'dotenv'
 config()
 import Client from '@replit/database'
 
-if (process.env?.REPLIT_DB_URL == undefined) throw new Error('REPLIT_DB_URL is not defined')
-if (process.env?.ADMIN_PASSWORD == undefined) throw new Error('ADMIN_PASSWORD is not defined')
-
 export default class Global {
-  private static _db = new Client(process.env?.REPLIT_DB_URL)
-  private static _adminPassword = process.env.ADMIN_PASSWORD
+  private static _db = new Client(process.env?.REPLIT_DB_URL ?? Global.err('REPLIT_DB_URL is not defined')) 
+  private static _adminPassword = process.env.ADMIN_PASSWORD ?? Global.err('ADMIN_PASSWORD is not defined')
 
   static get db() {
     return Global._db
@@ -20,5 +17,9 @@ export default class Global {
   }
   static set adminPassword(adminPassword) {
     throw new Error('cannot set this property')
+  }
+
+  private static err(msg:string):never{
+    throw new Error(msg)
   }
 }
