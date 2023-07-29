c00a235c8d33c1a5e6d4b050cece83826d6718ce
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-29 12:34:24 -0600

    formatting

--- app/server/Global.ts
+++ app/server/Global.ts
@@ -1,27 +1,24 @@
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 config()
 import Client from '@replit/database'
 
-if(process.env?.REPLIT_DB_URL == undefined) throw new Error('REPLIT_DB_URL is not defined')
-if(process.env?.ADMIN_PASSWORD == undefined) throw new Error('ADMIN_PASSWORD is not defined')
+if (process.env?.REPLIT_DB_URL == undefined) throw new Error('REPLIT_DB_URL is not defined')
+if (process.env?.ADMIN_PASSWORD == undefined) throw new Error('ADMIN_PASSWORD is not defined')
 
-export default class Global{
+export default class Global {
   private static _db = new Client(process.env?.REPLIT_DB_URL)
   private static _adminPassword = process.env.ADMIN_PASSWORD
 
-  static get db(){
+  static get db() {
     return Global._db
   }
-
-  static set db(db){
+  static set db(db) {
     throw new Error('cannot set this property')
   }
-
-  static get adminPassword(){
-    return Global._adminPassword 
+  static get adminPassword() {
+    return Global._adminPassword
   }
-
-  static set adminPassword(adminPassword){
+  static set adminPassword(adminPassword) {
     throw new Error('cannot set this property')
   }
 }
