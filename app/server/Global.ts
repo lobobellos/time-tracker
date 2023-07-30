510743010f0df7cde0c688e3c96e9986450c3d57
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-29 18:43:01 -0600

    undo my arrogant stupidity

--- app/server/Global.ts
+++ app/server/Global.ts
@@ -3,21 +3,8 @@ config()
 import Client from '@replit/database'
 
 export default class Global {
-  private static _db = new Client(process.env?.REPLIT_DB_URL ?? Global.err('REPLIT_DB_URL is not defined')) 
-  private static _adminPassword = process.env.ADMIN_PASSWORD ?? Global.err('ADMIN_PASSWORD is not defined')
-
-  static get db() {
-    return Global._db
-  }
-  static set db(db) {
-    throw new Error('cannot set this property')
-  }
-  static get adminPassword() {
-    return Global._adminPassword
-  }
-  static set adminPassword(adminPassword) {
-    throw new Error('cannot set this property')
-  }
+  static readonly db = new Client(process.env?.REPLIT_DB_URL ?? Global.err('REPLIT_DB_URL is not defined')) 
+  static readonly adminPassword = process.env.ADMIN_PASSWORD ?? Global.err('ADMIN_PASSWORD is not defined')
 
   private static err(msg:string):never{
     throw new Error(msg)
