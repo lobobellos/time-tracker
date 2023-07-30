96c680e42d6a9a5d802c4218cc725e8ff6273596
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 17:29:11 -0600

    switch to script setup

--- app/pages/Admin.vue
+++ app/pages/Admin.vue
@@ -2,94 +2,85 @@
 	<h2>Admin</h2>
 	<label for="adminPass">admin password</label>
 	<br />
-	<input
-		name="adminPass"
-		type="password"
-		v-model="adminPass"
-		@input="checkPass"
-	/>
-
-	<div v-if="correctPass">
+	<form @submit.prevent="e => correctPass.refresh()">
+		<input
+			name="adminPass"
+			type="password"
+			v-model="adminPass"
+		/>
+		<button>check</button>
+	</form>
+	<div v-if="correctPass.data.value">
 		<textarea name="" id="" v-model="textArea"></textarea>
 		<button @click="publishData">submit new data</button>
 	</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import beautify from 'json-beautify'
 
-export default {
-	data() {
-		return {
-			adminPass: '',
-			correctPass: false,
-			textArea: '',
+const adminPass = ref('')
+const textArea = ref('')
+
+const correctPass = useAsyncData(
+	'isPassCorrect',
+	async () => {
+		return await $fetch('/api/admin/checkPass', {
+			method: 'POST',
+			body: {
+				password: adminPass.value,
+			},
+		}).then(async res => {
+			if (res) fullData.refresh()
+			console.log('correct?', res)
+			return res
+		})
+	},
+)
+
+const fullData = useAsyncData('fulluserInfo', async () => {
+	$fetch('/api/admin/fullData', {
+		method: 'GET',
+		headers: [['admin-password', adminPass.value]],
+	}).then(async res => {
+		if (res.ok) {
+			alert(res.message)
+			textArea.value = beautify(res?.data, [], 2, 1e2)
 		}
-	},
-	computed: {
-		prettyJson() {
-			return beautify(this.data, null, 2, 1e2)
-		},
-	},
-	methods: {
-		async checkPass() {
-			this.correctPass = this.correctPass
-				? true
-				: await fetch('/isAdminPassword', {
-						method: 'POST',
-						headers: [['Content-Type', 'application/json']],
-						body: JSON.stringify({
-							password: this.adminPass,
-						}),
-				  }).then(res => res.json())
-			if (this.correctPass) {
-				this.getAllData()
-			}
-		},
-		async getAllData() {
-			await fetch('/fullData', {
-				method: 'GET',
-				headers: [['admin-password', this.adminPass]],
-			}).then(async res => {
-				if (res.ok) {
-					this.data = await res.json()
-					this.textArea = beautify(this.data, null, 2, 1e2)
-				} else {
-					alert(await res.text())
-				}
-			})
-		},
-		async publishData() {
-			try {
-				JSON.parse(this.textArea)
-			} catch (err) {
-				alert('invalid json')
-				return
-			}
-			if (
+		console.log('fullData', res)
+	})
+})
+
+async function publishData() {
+	try {
+		JSON.parse(textArea.value)
+	} catch (err) {
+		alert('invalid json')
+		return
+	}
+	if (
+		['yes', 'y', 'ye', 'yeah'].includes(
+			String(
 				prompt(
 					"are you sure? type 'yes' to confirm",
-				).toLowerCase() == 'yes'
-			) {
-				await fetch('/publishData', {
-					method: 'POST',
-					headers: [
-						['Content-Type', 'application/json'],
-						['admin-password', this.adminPass],
-					],
-					body: JSON.stringify({
-						data: JSON.parse(this.textArea),
-					}),
-				}).then(async res => {
-					if (res.ok) {
-						alert('data published')
-					} else {
-						alert('Error' + (await res.text()))
-					}
-				})
+				)?.toLowerCase(),
+			),
+		)
+	) {
+		await $fetch('/api/admin/setData', {
+			method: 'POST',
+			body: {
+				password: adminPass.value,
+				data: JSON.parse(textArea.value),
+			},
+		}).then(async res => {
+			if (res.ok) {
+				alert('data published')
+			} else {
+				alert('Error' + res.message)
 			}
-		},
-	},
+		})
+	}
 }
 </script>
 
