53d1c24fba4ce7a71843b41c508b502e704d3cb3
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 17:29:26 -0600

    mongoose rewrite

--- app/server/api/create.post.ts
+++ app/server/api/create.post.ts
@@ -1,20 +1,49 @@
-import { addUser } from "../dataManager";
+import users from '../dbModels/user'
+interface IRequestBody {
+  pin: number
+  name: string
+  title:string
+}
 
 export default defineEventHandler(async (event) => {
-  const {pin, name, title} = await readBody(event)
+  const {pin, name, title} = await readBody<IRequestBody>(event)
   console.log("now account creation occuring", pin, name, title)
-  try{
-    await addUser(pin, name, title);
-    setCookie(event, "pin", pin)
-    return {
-      ok: true,
-      message: "account created"
-    }
-  }catch(err){
-    return {
-      ok: false,
-      message: String(err)
-    }
-  }
   
+  try {
+    const userData = await users.findOne({
+      pin:pin
+    });
+    console.log("users with same pin: ",userData)
+    if (userData) {
+      console.log(`User with pin ${pin} already exists`);
+      return {
+        ok:false,
+        code: "USER_EXISTS",
+        message: "User with given pin already exists.",
+      };
+    } else {
+      console.log("Create user");
+      const newUserData = await users.create({
+        pin,
+        name,
+        title,
+        bio:'',
+        roomTimes:[]
+      });
+      
+      setCookie(event, "pin", pin.toString())
+      return {
+        ok: true,
+        message: "account created"
+      }
+    }
+  } catch (err) {
+    return {
+      ok:false,
+      message: String(err),
+    };
+  }
 })
+
+  
+
