53067eeed6573119f0eb944dca0aacd8ec58fd19
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 17:30:53 -0600

    switch to runtimeconfig to store envs

--- app/server/api/admin/checkPass.post.ts
+++ app/server/api/admin/checkPass.post.ts
@@ -1,6 +1,4 @@
-import Global from "../../Global"
-
 export default defineEventHandler(async (event) => {
   const {password} =await readBody(event)
-  return Global.adminPassword === password
+  return useRuntimeConfig().adminPassword === password
 })
