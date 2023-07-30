f01096fecb247727fbd78301daa8c03b215fe084
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 17:25:09 -0600

    add mongodb

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,7 +1,17 @@
+import {config} from 'dotenv'
+config()
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   //devtools: { enabled: true },
   routeRules: {
     '*': {ssr: false}
-  }
+  },
+  runtimeConfig: {
+    mongoUrl: process.env.MONGO_URL,
+    adminPassword : process.env.ADMIN_PASSWORD
+  },
+  nitro: {
+    plugins: ["~/server/index.ts"],
+  },
 })
