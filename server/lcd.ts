84c449eacb3dfc651062083eec2d53fad05c3457
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-31 12:16:34 -0600

    rewrite as a staic class

--- server/lcd.ts
+++ server/lcd.ts
@@ -1,51 +1,26 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
+import LCD from 'lcd'
 export default class Lcd {
   static initialized = false
-  private ip = 'undefined'
-  private cur = 0
-  private line2 = ""
-  constructor() {
-    if (!Lcd.initialized) {
-      Lcd.initialized = true
-      this.init()
-    } else {
-      throw new Error('Lcd already initialized')
-    }
+  static ready = false
+  private static line1 = ''
+  private static line2 = ""
+  private static lcd =new LCD({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 })
+  private static async init() {
+    this.lcd.on('ready', async () => {
+      setInterval(async () => {
+        await this.lcd.clear();
+        this.lcd.setCursor(0, 0);
+        this.lcd.print(this.line1, () => {
+          this.lcd.setCursor(0, 1);
+          this.lcd.print(this.line2 ?? "", () => {})
+        });
+      }, 1000)
+    })
   }
-  async init() {
-    if (process.env.IS_PROD == 'true') {
-      //@ts-ignore
-      const Lcd = (await import('lcd')).default
-      const lcd = new Lcd({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 });
-      lcd.on('ready', async () => {
-        setInterval(async () => {
-          await lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print(this.getParsedString(), () => {
-            lcd.setCursor(0, 1);
-            lcd.print(this.line2 ?? "", () => { })
-          });
-          this.cur = (this.cur + 1) % (this.getFullString().length - 16)
-        }, 1000)
-      })
-    }
-    
+  set line1(line: string) {
+    this.line1 = line.substring(0,16)
   }
-  getFullString() {
-    return `go to ${this.ip}`
-  }
-
-  getParsedString() {
-    return this.getFullString().substring(this.cur, this.cur + 16)
-  }
-
-  setIP(ip: string) {
-    this.ip = ip
-  }
-
-  setLine2(line2: string) {
-    this.line2 = line2
+  set line2(line: string) {
+    this.line2 = line.substring(0,16)
   }
 }
