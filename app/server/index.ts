85834eda0a10b616c273dc2a7dbed7c52d2a5ed5
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 22:16:18 -0600

    use fancy destructuring

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-const config = useRuntimeConfig();
+const {mongoUrl} = useRuntimeConfig();
 export default async () => {
   try {
-    console.log('config url',config.mongoUrl)
-    await mongoose.connect(config.mongoUrl);
+    console.log('config url',mongoUrl)
+    await mongoose.connect(mongoUrl);
     console.log("DB connection established.");
   } catch (err) {
     console.error("DB connection failed.", err);
