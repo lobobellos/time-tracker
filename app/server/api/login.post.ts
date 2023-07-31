9aadaae48d6b5497cc7e05cbbfd0a3f2a770a1be
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-07-30 21:42:43 -0600

    rewrite for mongoose

--- app/server/api/login.post.ts
+++ app/server/api/login.post.ts
@@ -1,13 +1,15 @@
-import { getRawData } from "../dataManager";
+import users from "../dbModels/user"
 
 export default defineEventHandler(async (event) => {
   console.log('user login request')
   const { pin } = await readBody(event)
 
-  const data = (await getRawData())
-    .find(el => el[0] == pin)
+  const data = await users.findOne({
+    pin,
+  });
   if (data) {
-    setCookie(event, "pin", pin)
+    setCookie(event, "id", data._id.toString())
+    console.log('id set', data._id.toString())
     return {
       ok: true,
       message: "login successful"
