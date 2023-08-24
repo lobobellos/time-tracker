9bceb5bd2e3e383271eef4e4d18ad0c049645b49
Replit user <>
2023-08-24 16:28:18 +0000

    initialize LCD

--- client/index.ts
+++ client/index.ts
@@ -1,9 +1,17 @@
 import chalk from "chalk"
 import {init} from  './inputListener.js'
+import Lcd from  './lcd.js'
 
 console.log(chalk.blueBright("input listener starting"))
 init().then(()=>{
   console.log(chalk.greenBright('input listener started succesfully!!'))
-}).catch(
-  console.error
-)
+}).catch(err=>{
+  console.log(err)
+})
+
+console.log(chalk.blueBright("LCD starting"))
+Lcd.init().then(()=>{
+  console.log(chalk.greenBright('LCD started succesfully!!'))
+}).catch(err=>{
+	console.log(err)
+})
