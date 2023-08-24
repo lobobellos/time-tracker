757d2d56bfb4227046df18077fc8ebb3a0881751
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-24 09:11:15 -0600

    remove getter wierdness

--- client/lcd.ts
+++ client/lcd.ts
@@ -1,30 +1,22 @@
 import LCD from 'lcd'
 export default class Lcd {
-  static initialized = false
-  static ready = false
+  static readonly ready = false;
+
+  static line1 = "";
+  static line2 = "";
+  
   private static lcd =new LCD({ rs: 26, e: 19, data: [13, 6, 5, 11], cols: 16, rows: 2 })
   private static async init() {
     this.lcd.on('ready', async () => {
       setInterval(async () => {
         await this.lcd.clear();
         this.lcd.setCursor(0, 0);
-        this.lcd.print(this.line1, () => {
+        this.lcd.print(this.line1 ?? "", () => {
           this.lcd.setCursor(0, 1);
           this.lcd.print(this.line2 ?? "", () => {})
         });
       }, 1000)
     })
   }
-  static set line1(line: string) {
-    this.line1 = line.substring(0,16)
-  }
-  static get line1():never {
-    throw new Error("line1 is private")
-  }
-  static set line2(line: string) {
-    this.line2 = line.substring(0,16)
-  }
-  static get line2():never {
-    throw new Error("line2 is private")
-  }
+  
 }
