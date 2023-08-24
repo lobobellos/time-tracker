cda834b9079f52f62430a84249e9f418576b79e1
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-24 09:44:16 -0600

    revert stupidity

--- client/inputListener.ts
+++ client/inputListener.ts
@@ -1,9 +1,7 @@
 import Global from './Global.js'
 import gpio from 'rpi-gpio'
 import Lcd from './lcd.js'
-
 type time = [number,number]
-
 export async function init() {
   let lastPin: null | number = null
   let tempPin = ""
@@ -12,9 +10,7 @@ export async function init() {
   
   await gpio.promise.setup(7, gpio.DIR_IN, gpio.EDGE_RISING)
   await gpio.promise.setup(11, gpio.DIR_IN, gpio.EDGE_RISING)
-
   Lcd.line1 = 'potato';
-
   gpio.on('change', async rpipin => {
     if (pressedRecently) return;
     pressedRecently = true
@@ -32,85 +28,28 @@ export async function init() {
       }
     } else if (rpipin == 11) {
       //clock out
-      if ( clockedIn.has(lastPin) ) {
+      if (lastPin && clockedIn.has(lastPin) ) {
         const time: time = [
           <number>clockedIn.get(lastPin),
           Date.now()
         ]
-        if (isvalid(time)){
-          fetch(Global.prodUrl+"/api/client/addTime", {
-            method: "POST",
-            headers: [['Content-Type', 'application/json']],
-            body: JSON.stringify({
-              time,
-              pin:lastPin
-            })
-          }).then(async res => {
-            if (res.ok) {
-              console.log("login succesfull")
-              Lcd.line1 = "login succesfull"
-            }else{
-              console.log("failure: "+ await res.text())
-              Lcd.line1 = await res.text()
-            }
+        if (isvalid(time)) fetch(Global.prodUrl+"/api/client/addTime", {
+          method: "POST",
+          headers: [['Content-Type', 'application/json']],
+          body: JSON.stringify({
+            time,
+            pin:lastPin
           })
-        }else{
-          console.log("invalid time"
-        }
+        }).then(async res => {
+          if (res.ok) {
+            console.log("login succesfull")
+            Lcd.line1 = "login succesfull"
+          }else{
+            console.log("failure: "+ await res.text())
+            Lcd.line1 = await res.text()
+          }
+        })
+        else console.log("invalid time");
       }
     }
   })
-
-  const KEYSTATETABLE = {
-    "<KP0>": 0,
-    "<KP1>": 1,
-    "<KP2>": 2,
-    "<KP3>": 3,
-    "<KP4>": 4,
-    "<KP5>": 5,
-    "<KP6>": 6,
-    "<KP7>": 7,
-    "<KP8>": 8,
-    "<KP9>": 9,
-  }
-
-  //@ts-ignore
-  const GK = (await import("global-keypress")).default
-  const gk = new GK();
-
-  // launch keypress daemon process
-  gk.start();
-
-  gk.on('press', ({ data }: any) => {
-    console.log(data)
-
-    if (data == "<KPEnter>" && tempPin != '') {
-      lastPin = parseInt(tempPin)
-      tempPin = ""
-      setTimeout(() => lastPin = null, 10_000)
-    } else {
-      tempPin += (parseInt(KEYSTATETABLE[data] ?? "") || "").toString()
-    }
-    console.log(tempPin)
-    console.log(lastPin)
-  })
-
-  // process error
-  gk.on('error', error => {
-    console.error(error);
-  });
-
-  // process closed
-  gk.on('\nkeylistener closing\n', () => {
-    console.log('closed');
-  });
-}
-
-function isvalid(t: time): boolean {
-  let [start, end] = t
-  const day = 24 * 60 * 60 * 1000
-  return (new Date(start)).getDay() == (new Date(end)).getDay() &&
-    end - start < day
-}
-
-
