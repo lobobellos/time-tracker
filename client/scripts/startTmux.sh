ba2429dbd4603679effc6e24db10e58f050dde39
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-08-24 08:50:34 -0600

    Update startTmux.sh

--- client/scripts/startTmux.sh
+++ client/scripts/startTmux.sh
@@ -2,6 +2,6 @@
 
 sudo tmux new -d -s timeTracker
 
-sudo tmux send-keys -t timeTracker:0 cd Space /home/pi/time-tracker/server/ C-m
+sudo tmux send-keys -t timeTracker:0 cd Space /home/pi/time-tracker/client/ C-m
 sudo tmux send-keys -t timeTracker:0 sudo Space npm Space run Space start C-m
 echo "started tmux"
